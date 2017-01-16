070466b8d25535c96688d20ccadb835de0087243
Manvir Kaur <manvkaur@microsoft.com>
2017-01-16 12:28:48 +0530

    Expense Manager Base files

---
+++ ExpenseManager/src/ExpenseManager.Client/common/expenseResource.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    "use strict";
+    var app = angular.module("expenseManager")
+                     .factory("expenseResource",
+                             ["$resource",
+                             "appSettings",
+                             expenseResource]);
+    function expenseResource($resource, appSettings) {
+        return $resource(appSettings.serverPath + "/api/expensereport/:id")
+    }
+}());
